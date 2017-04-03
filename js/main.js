32c2e878698e1603fbbcbadd43168e971e3527c1
Justin <justin.strayhorn@hotmai.com>
2017-04-03 17:44:52 -0500

    added styling to moon element on click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+// Toggle between Sun and Moon on click
   $('#star').click(function() {
     if ($('#star').hasClass('sun')) {
       $('#star').removeClass('sun').addClass('moon');
@@ -7,11 +7,31 @@ $(document).ready(function() {
     else {
       $('#star').removeClass('moon').addClass('sun');
     };
+    // Toggle body background color on click
     if ($('#sky').hasClass('day')) {
         $('#sky').removeClass('day').addClass('night');
     }
     else {
         $('#sky').removeClass('night').addClass('day');
+    };
+    // Toggle Moon Spots on click
+    if ($('#spot1').hasClass('visible')) {
+        $('#spot1').removeClass('visible');
+    }
+    else {
+      $('#spot1').addClass('visible');
+    };
+    if ($('#spot2').hasClass('visible')) {
+        $('#spot2').removeClass('visible');
+    }
+    else {
+      $('#spot2').addClass('visible');
+    };
+    if ($('#spot3').hasClass('visible')) {
+        $('#spot3').removeClass('visible');
+    }
+    else {
+      $('#spot3').addClass('visible');
     };
   });
 });
