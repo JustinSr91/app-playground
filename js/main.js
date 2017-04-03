46de6b2ce53f9d667a9ed6f27e50a27c79e84cfc
Justin <justin.strayhorn@hotmai.com>
2017-04-02 23:48:26 -0500

    created sun and moon elements functionality toggle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-  
+
+  $('#star').click(function() {
+    if ($('#star').hasClass('sun')) {
+      $('#star').removeClass('sun').addClass('moon');
+    }
+    else {
+      $('#star').removeClass('moon').addClass('sun');
+    }
+
+  $('#sky').click(function() {
+    if ($('#sky').hasClass('day')) {
+        $('#sky').removeClass('day').addClass('night');
+    }
+    else {
+        $('#sky').removeClass('night').addClass('day');
+    }
+  })
+  });
 });
